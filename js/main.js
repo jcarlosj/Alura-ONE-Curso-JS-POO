31d0f44de472511c0f82bdf18b3e7f5f6958bdd0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-10-14 11:49:00 -0500

    [+] Class: CuentaBanco

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert( 'Cuenta de Banco' );
+class CuentaBanco {
+    nombreTitular;
+    numeroCuenta;
+    saldo = 0;
+    fechaApertura = new Date();
+}
+
+const cuentaJuan = new CuentaBanco();
+
+cuentaJuan.nombreTitular = 'Juan';
+cuentaJuan.numeroCuenta = '986654353-0';
+cuentaJuan.saldo = 100;
+
+console.log( cuentaJuan );
